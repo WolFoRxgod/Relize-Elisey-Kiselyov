748611cbc41d5ab219917e7fe8f85d210337a8a4
VikaKhaustova <haustovavika@gmail.com>
2024-10-07 17:14:07 +0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Отримуємо кнопку "Кошик"
+let cartBtn = document.getElementById('cartBtn')
 
+// Навішуємо обробник подій на клік кнопки "Кошик"
+cartBtn.addEventListener("click", function () {
+    // Переходимо на сторінку кошика
+    window.location.assign('cart.html')
+})
